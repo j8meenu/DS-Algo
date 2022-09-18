7ea309e63df0001bcdfbd4ea51bc510b246896b1
meenu <j8meenu@gmail.com>
2022-09-18 19:45:03 -0400

    'metadata'

--- DSAlgo/src/test/java/numpyninja/Tests/HomeTest.java
+++ DSAlgo/src/test/java/numpyninja/Tests/HomeTest.java
@@ -1,5 +1,39 @@
 package numpyninja.Tests;
 
-public class HomeTest {
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import numpyninja.Base.BaseTest;
+import numpyninja.Pages.HomePage;
+import numpyninja.Pages.LandingPage;
+
+public class HomeTest  {
+	
+	WebDriver driver;
+	HomePage pagefactory;
+	
+	@BeforeTest
+	public void beforetest() {
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		driver.get("https://dsportalapp.herokuapp.com/home");
+	}
+	
+	@Test
+	public void dropdownclick() {
+		//Homepage page=new Homepage(driver);
+		//		page.dropdown();
+		pagefactory=new HomePage(driver);
+		pagefactory.dropdown();
+	}
+	
+	
 
 }
--- DSAlgo/src/test/java/numpyninja/Tests/SignInTest.java
+++ DSAlgo/src/test/java/numpyninja/Tests/SignInTest.java
@@ -1,5 +1,48 @@
 package numpyninja.Tests;
 
 public class SignInTest {
+	 
+	WebDriver driver;
+	 SignInPage SignIn;
+	 String Uname = "Nenyarajesh";
+	 String Upwd ="Pinky1213";
+	 @BeforeTest
+		public void SignIn() {
+				System.setProperty("webdriver.chrome.driver", "./src/test/resources/drivers/chromedriver.exe");
+				driver = new ChromeDriver();
+				driver.get("https://dsportalapp.herokuapp.com/login");
+				driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		}
+				 @Test(priority=0)
+			       public void SignInwithoutentries() {
+			       	SignIn =new SignInPage(driver);
+			       	SignIn.clickLogin();
+			      	assertEquals(SignIn.nousername(), "Please fill out this field.");
+			       }
+			       @Test(priority=1)
+			       public void EmptyPassword() {
+			       	 SignIn =new SignInPage(driver);
+			            SignIn.nopwd(Uname);
+			            SignIn.clickLogin(); 
+			            assertEquals(SignIn.nopwd(Uname), "Please fill out this field.");
+			       }
+			       @Test(priority=2)
+			       public void dslogin() {
+			       	
+			       	SignIn.login(Uname,Upwd);
+			       	SignIn.clickLogin();
+			       	String actualUrl= "https://dsportalapp.herokuapp.com/home";
+			       	String expectedUrl= driver.getCurrentUrl();
+			       	Assert.assertEquals(actualUrl, expectedUrl);
+			       	if(actualUrl.equalsIgnoreCase(expectedUrl)) { 
+			       		System.out.println("Test passed"); }
+			       	else { System.out.println("Invalid credentials"); }
+			       	SignIn.clicklogout();
+			       }
+			       			      
+			       @AfterTest
+			       public void logout() {
+			    	 	driver.close();
+			       }
 
 }

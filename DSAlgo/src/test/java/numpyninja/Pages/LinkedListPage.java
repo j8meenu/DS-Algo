94618bd3cb98d373859abea3e4ac1f75c6527dda
varsha <varshasp2014@gmail.com>
2022-09-19 12:54:00 -0400

    Commiting LinkedListPage

--- DSAlgo/src/test/java/numpyninja/Pages/LinkedListPage.java
+++ DSAlgo/src/test/java/numpyninja/Pages/LinkedListPage.java
@@ -1,5 +1,82 @@
 package numpyninja.Pages;
 
-public class LinkedListPage {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
+import numpyninja.Base.BaseTest;
+
+public class LinkedListPage extends BaseTest  {
+
+	 WebDriver driver;
+	 
+	 public LinkedListPage(WebDriver driver) {
+		 
+		 this.driver = driver;
+		 PageFactory.initElements(driver, this);
+	 }
+	 
+	 
+	 @FindBy(xpath ="//div[3]/div[3]/div//a")      WebElement getStartedBtn;
+	 @FindBy(linkText = "Introduction")            WebElement introduction;
+	 @FindBy(xpath="//a[@href='/tryEditor']")      WebElement tryHereLink;
+	 @FindBy(xpath = "//textarea[@tabindex='0']")  WebElement codeTextArea;
+	 @FindBy(xpath = "//button")                   WebElement runButton;
+	 @FindBy(linkText ="Creating Linked LIst")     WebElement creatingLinkedList;
+	 @FindBy(linkText = "Types of Linked List")    WebElement typeOfLinkedList;
+	 @FindBy(linkText = "Implement Linked List in Python") WebElement implementLinkedList;
+	 @FindBy(linkText = "Traversal")     WebElement traversal;
+	 @FindBy(linkText = "Insertion")     WebElement insertion;
+	 @FindBy(linkText ="Deletion")       WebElement deletion;
+	 @FindBy(linkText ="Practice Questions")   WebElement practiceQuestion;
+	 @FindBy(xpath = "//div[@class='col-sm']//a") WebElement practQuesTryHereLink;
+	 
+	 public void getStartedButton()
+	 {
+	    getStartedBtn.click();
+	 }
+	 public void introButton() 
+	 {
+		 introduction.click();
+	 }
+	 public void tryhereBox() 
+	 {
+		 tryHereLink.click();
+		 codeTextArea.sendKeys("print('Hello World')");
+		 runButton.click();
+	 }
+	 public void createLinkedListBtn() 
+	 {
+		 driver.navigate().back();
+		 creatingLinkedList.click();
+	 }
+	 public void typeOfLinkedListBtn() {
+		 
+		 driver.navigate().back();
+		 typeOfLinkedList.click();
+	 }
+	 public void implementLinkedListBtn() {
+		 driver.navigate().back();
+		 implementLinkedList.click();
+	 }
+	 public void traversalBtn() {
+		 driver.navigate().back();
+		 traversal.click();
+	 }
+	 public void insertionBtn() {
+		 driver.navigate().back();
+		 insertion.click();
+	 }
+	 public void deletionBtn() {
+		 driver.navigate().back();
+		 deletion.click();
+	 }
+	 public void practiceQuestionBtn() {
+		 driver.navigate().back();
+		 practiceQuestion.click();	 
+		 
+	 }
+	 
 }
+
--- DSAlgo/src/test/java/numpyninja/Tests/LinkedListTest.java
+++ DSAlgo/src/test/java/numpyninja/Tests/LinkedListTest.java
@@ -1,5 +1,84 @@
 package numpyninja.Tests;
 
-public class LinkedListTest {
+import static org.testng.Assert.assertTrue;
 
+import java.io.IOException;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import numpyninja.Base.BaseTest;
+import numpyninja.Pages.LinkedListPage;
+import numpyninja.Pages.SignInPage;
+
+
+public class LinkedListTest extends BaseTest {
+	
+	 LinkedListPage linkedList;
+	 
+       @BeforeTest
+		public void setUp() throws IOException  {
+	    
+          initDriver();
+	      driver.get(loginUrl);
+	      
+	      SignInPage SignIn =new SignInPage(driver);
+	      SignIn.login(UserName, Password);
+	      SignIn.clickLogin();
+	  }
+       @Test
+       public void introLinkedlist() {
+       	linkedList = new LinkedListPage(driver);
+       	linkedList.getStartedButton();
+       	linkedList.introButton();
+       	linkedList.tryhereBox();
+       	String title = driver.getTitle();
+       	assertTrue(true, title);
+       	
+       }
+       @Test
+       public void creatingLinkedList()  {
+       	linkedList.createLinkedListBtn();
+       	linkedList.tryhereBox();
+       	String title = driver.getTitle();
+       	assertTrue(true, title);
+       }
+       @Test
+       public void typeOfLinkedList() {
+       	linkedList.typeOfLinkedListBtn();
+       	String title = driver.getTitle();
+       	linkedList.tryhereBox();
+       	assertTrue(true, title);
+       	
+       }
+       @Test
+       public void implementLinkedList() {
+       	
+       	linkedList.implementLinkedListBtn();
+       	linkedList.tryhereBox();
+       	String title = driver.getTitle();
+       	assertTrue(true, title);
+       }
+       @Test
+       public void traversal() {
+       	
+       	linkedList.traversalBtn();
+       	linkedList.tryhereBox();
+       	String title = driver.getTitle();
+       	assertTrue(true, title);
+       }
+       @Test
+       public void deletion() {
+       	
+       	linkedList.deletionBtn();
+       	linkedList.tryhereBox();
+       	String title = driver.getTitle();
+       	assertTrue(true, title);
+       }
+       @Test
+       public void practiceQuestion() {
+       	
+       	linkedList.practiceQuestionBtn(); 
+       	String title = driver.getTitle();
+       	assertTrue(true, title);
+       }      
 }

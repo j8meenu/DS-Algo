c025f508806d57bf745b8e433c32f72e527f50d2
sangavi-git91 <sangavi91s@gmail.com>
2022-09-21 22:06:52 -0400

    commiting home and ds page

--- DSAlgo/src/test/java/numpyninja/Tests/DataStructureTest.java
+++ DSAlgo/src/test/java/numpyninja/Tests/DataStructureTest.java
@@ -1,34 +1,87 @@
 package numpyninja.Tests;
 
+import static org.testng.Assert.assertTrue;
+
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import numpyninja.Base.BaseTest;
 import numpyninja.Pages.DataStructurePage;
+import numpyninja.Pages.HomePage;
+import numpyninja.Pages.SignInPage;
 
 
-public class DataStructureTest {
+public class DataStructureTest extends BaseTest {
 	
-	WebDriver driver;
-	DataStructurePage pagefactory;
-	
+	//WebDriver driver;
+	DataStructurePage ds;
+	HomePage home;
+	SignInPage signin;
+	BaseTest base;
 	@BeforeTest
-	public void beforetest() {
-		WebDriverManager.chromedriver().setup();
-		driver = new ChromeDriver();
-		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-		driver.get("https://dsportalapp.herokuapp.com/home");
+	public void beforetest() throws IOException {
+		
+			initDriver();
+	      driver.get(homeUrl);
+	      
+//		WebDriverManager.chromedriver().setup();
+//		driver = new ChromeDriver();
+//		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+//		driver.get("https://dsportalapp.herokuapp.com/home");
+	}	
+		@Test(priority=0)
+		public void introDatastructure() {
+		home=new HomePage(driver);
+	home.signinvalidation();
+		signin=new SignInPage(driver);
+		signin.login("Nenyarajesh", "Pinky1213");
+	signin.clickLogin();
+	home.getstartedDatastructureclick();
+	String title = driver.getTitle();
+   	assertTrue(true, title);
 	}
 	
-	@Test
-	public void dropdownclick() {
+	
+	
+	@Test(priority=1)
+	public void TimeComplexity() {
 		
-		pagefactory=new DataStructurePage(driver);
-		pagefactory.Datastructure();
+		ds=new DataStructurePage(driver);
+		ds.TimeComplexityClick();
+		String title = driver.getTitle();
+       	assertTrue(true, title);
 	}
+	
+	@Test(priority=2)
+	public void PracticeQuestion() {
+		ds=new DataStructurePage(driver);
+		ds.PracticeQuestionclick();
+		driver.navigate().back();
+		JavascriptExecutor js=(JavascriptExecutor)driver; 
+	js.executeScript("window.scrollBy(0,900)");
+	String title = driver.getTitle();
+   	assertTrue(true, title);
+	}
+@Test(priority=3)
+public void TryHere() {
+	ds.tryhereclick();
+	String title = driver.getTitle();
+   	assertTrue(true, title);
+}
+
+@Test(priority=4)
+public void TryEditor() {
+	ds.TryEditorClick();
+	driver.navigate().back();
+	String title = driver.getTitle();
+   	assertTrue(true, title);
+}
 
 }
--- DSAlgo/src/test/java/numpyninja/Tests/SignInTest.java
+++ DSAlgo/src/test/java/numpyninja/Tests/SignInTest.java
@@ -1,5 +1,18 @@
 package numpyninja.Tests;
 
+import static org.testng.Assert.assertEquals;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import numpyninja.Pages.SignInPage;
+
 public class SignInTest {
 	 
 	WebDriver driver;
